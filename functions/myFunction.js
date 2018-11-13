c2aa4b3c74f9cc323e680e3761f76daaedb594dd
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-13 04:43:21 -0600

    Update myFunction.js

--- functions/myFunction.js
+++ functions/myFunction.js
@@ -1,6 +1,12 @@
 exports.handler = function(event, context, callback) {
+
+    var bodyO = {
+      nombre: 'Joseph',
+      edad: 25
+    };
+
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: JSON.stringify(bodyO)
     });
 }
