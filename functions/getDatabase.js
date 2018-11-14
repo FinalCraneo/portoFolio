905ebab22192a3e8bdc1d02f8ef20c45709b5706
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-14 05:43:23 -0600

    reduxx

--- functions/getDatabase.js
+++ functions/getDatabase.js
@@ -3,9 +3,9 @@
 var firebase = require('firebase');
 
 exports.handler = function(event, context, callback) {
-	context.callbackWaitsForEmptyEventLoop = false;  //<---Important
+    context.callbackWaitsForEmptyEventLoop = false; //<---Important
 
-	var config = {
+    var config = {
         apiKey: "AIzaSyCPsiU4bLGc1_18AVr3MNevz_OteSedi7Q",
         authDomain: "my-not-awesome-project-24bbf.firebaseapp.com",
         databaseURL: "https://my-not-awesome-project-24bbf.firebaseio.com",
@@ -14,13 +14,16 @@ exports.handler = function(event, context, callback) {
         messagingSenderId: "104276198773"
     };
 
-    if (firebase.apps.length == 0) {  // <---Important!!! In lambda, it will cause double initialization.
-    	firebase.initializeApp(config);
+    if (firebase.apps.length == 0) { // <---Important!!! In lambda, it will cause double initialization.
+        firebase.initializeApp(config);
     }
 
     let database = firebase.database();
 
-    console.log(database);
+    var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
+    var url = new URL(url_string);
+    var c = url.searchParams.get("c");
+    console.log('c: ', c);
 
     let hello = 'Hello';
 
