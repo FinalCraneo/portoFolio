1d83e01b85cb4807df87a5cae113470904677cb2
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-14 03:47:54 -0600

    firebase npm

--- functions/getDatabase.js
+++ functions/getDatabase.js
@@ -1,23 +1,30 @@
+'use strict';
+
+var firebase = require("firebase");
+
 exports.handler = function(event, context, callback) {
+	context.callbackWaitsForEmptyEventLoop = false;  //<---Important
 
+	var config = {
+        apiKey: "AIzaSyCPsiU4bLGc1_18AVr3MNevz_OteSedi7Q",
+        authDomain: "my-not-awesome-project-24bbf.firebaseapp.com",
+        databaseURL: "https://my-not-awesome-project-24bbf.firebaseio.com",
+        projectId: "my-not-awesome-project-24bbf",
+        storageBucket: "my-not-awesome-project-24bbf.appspot.com",
+        messagingSenderId: "104276198773"
+    };
 
-    //TODO
+    if (firebase.apps.length == 0) {  // <---Important!!! In lambda, it will cause double initialization.
+    	firebase.initializeApp(config);
+    }
 
-    // var config = {
-    //     apiKey: "AIzaSyCPsiU4bLGc1_18AVr3MNevz_OteSedi7Q",
-    //     authDomain: "my-not-awesome-project-24bbf.firebaseapp.com",
-    //     databaseURL: "https://my-not-awesome-project-24bbf.firebaseio.com",
-    //     projectId: "my-not-awesome-project-24bbf",
-    //     storageBucket: "my-not-awesome-project-24bbf.appspot.com",
-    //     messagingSenderId: "104276198773"
-    // };
-    // firebase.initializeApp(config);
+    let database = firebase.database();
 
-    // database = firebase.database();
+    console.log(database);
 
-    // let hello = 'Hello';
+    let hello = 'Hello';
 
-    // callback(null, hello);
+    callback(null, hello);
 
     var bodyO = {
         nombre: 'Joseph',
