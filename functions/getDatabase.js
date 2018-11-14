edb89c5d03dd7c10abc7cc8c427b2eae266a5c35
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-14 03:10:04 -0600

    Update functions/getDatabase.js

--- functions/getDatabase.js
+++ functions/getDatabase.js
@@ -1,17 +1,17 @@
 exports.handler = (event, contect, callback) => {
     //TODO
 
-    var config = {
-        apiKey: "AIzaSyCPsiU4bLGc1_18AVr3MNevz_OteSedi7Q",
-        authDomain: "my-not-awesome-project-24bbf.firebaseapp.com",
-        databaseURL: "https://my-not-awesome-project-24bbf.firebaseio.com",
-        projectId: "my-not-awesome-project-24bbf",
-        storageBucket: "my-not-awesome-project-24bbf.appspot.com",
-        messagingSenderId: "104276198773"
-    };
-    firebase.initializeApp(config);
+    // var config = {
+    //     apiKey: "AIzaSyCPsiU4bLGc1_18AVr3MNevz_OteSedi7Q",
+    //     authDomain: "my-not-awesome-project-24bbf.firebaseapp.com",
+    //     databaseURL: "https://my-not-awesome-project-24bbf.firebaseio.com",
+    //     projectId: "my-not-awesome-project-24bbf",
+    //     storageBucket: "my-not-awesome-project-24bbf.appspot.com",
+    //     messagingSenderId: "104276198773"
+    // };
+    // firebase.initializeApp(config);
 
-    database = firebase.database();
+    // database = firebase.database();
 
     let hello = 'Hello';
 
