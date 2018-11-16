938dfd8251c5843d835151987246cdc7e79eb7e5
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-16 01:39:05 -0600

    Update src/lambda/getDatabase.js

--- src/lambda/getDatabase.js
+++ src/lambda/getDatabase.js
@@ -1,9 +1,6 @@
 const axios = require('axios');
 
-// var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2"; //window.location.href
-// var url = new URL(url_string);
-// var c = url.searchParams.get("c");
-// console.log(c);
+
 
 exports.handler = (event, context, callback) => {
 
@@ -14,6 +11,7 @@ exports.handler = (event, context, callback) => {
     axios.get(`https://my-not-awesome-project-24bbf.firebaseio.com/users/${name}.json`)
         .then((res) => {
             res.data.params = event.queryStringParameters;
+            res.data.someVar = process.env.someVar;
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(res.data),
