eb6f47aacdfa2a54dde8f85063a53ea371bdf428
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-16 01:42:07 -0600

    Update src/lambda/getDatabase.js

--- src/lambda/getDatabase.js
+++ src/lambda/getDatabase.js
@@ -7,11 +7,12 @@ exports.handler = (event, context, callback) => {
     console.log('El nombre: ', event.queryStringParameters.name);
 
     let name = event.queryStringParameters.name;
+    let getUserUrl = process.env.getUserUrl;
 
-    axios.get(`https://my-not-awesome-project-24bbf.firebaseio.com/users/${name}.json`)
+    axios.get(`${getUserUrl}/${name}.json`)
         .then((res) => {
             res.data.params = event.queryStringParameters;
-            res.data.someVar = process.env.someVar;
+            res.data.siEntre = 'si entre';
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(res.data),
