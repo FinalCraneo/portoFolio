7eb679552eaf5578042913ba1bb6611f738e613c
FinalCraneo <44252641+FinalCraneo@users.noreply.github.com>
2018-11-16 19:40:34 -0600

    Update callback.js

--- src/lambda/callback.js
+++ src/lambda/callback.js
@@ -2,36 +2,40 @@ const axios = require('axios');
 
 exports.handler = (event, context, callback) => {
 
-        let client_id, redirect_uri, secret, code;
 
-        client_id = process.env.client_id;
-        redirect_uri = process.env.redirect_uri;
-        secret = process.env.secret;
+    let client_id, redirect_uri, secret, code;
 
-        code = event.queryStringParameters.code;
+    client_id = process.env.client_id;
+    redirect_uri = process.env.redirect_uri;
+    secret = process.env.secret;
 
-        let postUrl = `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${secret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}`;
+    code = event.queryStringParameters.code;
 
-        axios.post(postUrl)
-            .then(res => {
-                    console.log(res);
+    let postUrl = `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${secret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}`;
 
-                    axios.put('https://my-not-awesome-project-24bbf.firebaseio.com/users.json', {
-                            foo: bar,
-                            algo: res
-                        })
-                        .then(response => {
-                                // do something...
-                            }
+    axios.post(postUrl)
+        .then(res => {
+            console.log(res);
 
-                            callback(null, {
-                                statusCode: 301,
-                                headers: {
-                                    Location: 'https://kenopsia.ga/profile',
-                                }
-                            });
-                        })
-                .catch(err => {
-                    callback(err);
-                });
-            };
+            axios.put('https://my-not-awesome-project-24bbf.firebaseio.com/users.json', {
+                    foo: bar,
+                    algo: res
+                })
+                .then(response => {
+                    // do something...
+
+
+                    callback(null, {
+                        statusCode: 301,
+                        headers: {
+                            Location: 'https://kenopsia.ga/profile',
+                        }
+                    })
+
+                })
+
+        })
+        .catch(err => {
+            callback(err);
+        });
+};
